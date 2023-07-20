9d5caf9ed9e02f86219e3628a6c9712d84e8ee7e
Abdo <abdo.maxmud@gmail.com>
2023-07-20 19:09:13 +0500

    not found

---
+++ src/views/NotFound/NotFound.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<template>
+  <main>
+    <strong>404 Not found !</strong>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
