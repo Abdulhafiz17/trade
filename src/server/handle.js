702ef54f4ac5ad3a9e548030a9c0ddeb4ae8d01e
Abdo <abdo.maxmud@gmail.com>
2023-07-31 10:32:49 +0500

    handle 500 error

--- src/server/handle.js
+++ src/server/handle.js
@@ -5,7 +5,9 @@ import store from "../store";
 export default function handleError(error) {
   const status = error.response?.status;
   const detail = error.response?.data?.detail;
-  if (status)
+  if (error.code == "ERR_NETWORK") {
+    util.toast("warning", "Serverda hatolik !");
+  } else if (status)
     if (status == 400) {
       util.toast("warning", detail);
     } else if (status == 401) {
